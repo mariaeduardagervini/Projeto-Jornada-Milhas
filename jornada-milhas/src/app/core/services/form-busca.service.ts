0f82fb038c5ca287af32b3425b3e98a4b1d4d80a
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-08-31 14:56:26 -0300

    criação do formulario

---
+++ jornada-milhas/src/app/core/services/form-busca.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FormBuscaService {
+  formBusca: FormGroup;
+  constructor() {
+    this.formBusca = new FormGroup({});
+  }
+}
